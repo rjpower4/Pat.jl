53f8fc0e9ce0623bc31d501d09c43d9f94d89e03
Rolfe Power <rolfepower4@gmail.com>
2021-11-22 21:46:26 -0500

    add basic constants

---
+++ src/constants.jl
@@ -0,0 +1,69 @@
+"""
+    GRAVITATIONAL_CONSTANT
+
+Universal constant of gravitation. [km^3 / (kg . s^2)]
+
+Source: [NIST](https://physics.nist.gov/cgi-bin/cuu/Value?bg|search_for=universal_in!)
+"""
+const GRAVITATIONAL_CONSTANT = 6.6743e-20
+
+"""
+    ASTRONOMICAL_UNIT
+
+Astronomical unit as defined by the IAU. [km]
+
+Source: [IAU](https://www.iau.org/static/resolutions/IAU2012_English.pdf)
+"""
+const ASTRONOMICAL_UNIT = 149_597_870.700
+
+"""
+    EARTH_STANDARD_GRAVITY
+
+Standard gravitational acceleration at surface of Earth. [km/s^2]
+
+Source: [NIST](https://physics.nist.gov/cgi-bin/cuu/Value?gn|search_for=adopted_in!)
+"""
+const EARTH_STANDARD_GRAVITY = 9.80665e-03
+
+"""
+    PROTON_MASS
+
+Mass of the proton. [kg]
+
+Source: [NIST](https://physics.nist.gov/cgi-bin/cuu/Value?mp|search_for=atomnuc!)
+"""
+const PROTON_MASS = 1.67262192369e-27
+
+"""
+    NEUTRON_MASS
+
+Mass of the neutron. [kg]
+
+Source: [NIST](https://physics.nist.gov/cuu/Constants/Table/allascii.txt)
+"""
+const NEUTRON_MASS = 1.67492749804e-27
+
+"""
+    ELECTRON_MASS
+
+Mass of the electron. [kg]
+
+Source: [NIST](https://physics.nist.gov/cgi-bin/cuu/Value?mp|search_for=atomnuc!)
+"""
+const ELECTRON_MASS = 9.1093837015e-31
+
+"""
+    SPEED_OF_LIGHT
+
+Speed of light in a vacuum. [km/s]
+
+Source: [NIST](https://physics.nist.gov/cgi-bin/cuu/Value?c|search_for=adopted_in!)
+"""
+const SPEED_OF_LIGHT = 299792458e-3
+
+"""
+    BOLTZMANN_CONSTANT
+
+Boltzmann constant. [J/K]
+"""
+const BOLTZMANN_CONSTANT = 1.380649e-23
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,3 @@
 using SafeTestsets
 
-@safetestset "Placeholder Test Set" begin
-    using Test
-    using Pat
-    @testset "Placeholder test" begin
-        @test sqrt(4) == 2 
-    end 
-end
+@safetestset "Constants Test" begin include("testsets/test_constants.jl") end
