1e2fcc3075c7e5d98cf9b5f6dcffcb1f716a1651
Rolfe Power <rolfepower4@gmail.com>
2021-12-06 10:42:08 -0500

    Simple body implementation

--- src/body.jl
+++ src/body.jl
@@ -1,22 +1,23 @@
-struct Sphere
-    radius::Float64
-end
-Sphere() = Sphere(1)
-radius(s::Sphere) = s.radius
-volume(s::Sphere) = radius(s)^3 * (4//3) * π
-area(s::Sphere) = 4π * radius(s)^2
+"""
+    CelestialBody
 
+Struct representing a gravitating body with a NAIF ID code.
+"""
 struct CelestialBody
     gm::Float64
     naif_id::Int64
-    shape::Sphere
 end
-CelestialBody(gm) = CelestialBody(gm, -1, Sphere())
-CelestialBody(gm, naif_id, radius::Float64) = CelestialBody(gm, naif_id, Sphere(radius))
+
+"""
+    CelestialBody(gm)
+
+Construct a new [`CelestialBody`](@ref) from its gravitational parameter.
+"""
+CelestialBody(gm) = CelestialBody(gm, -1)
+
+"""
+    CelestialBody(gm, naif_id, radius::Float64) 
+"""
 gravitational_parameter(cb::CelestialBody) = cb.gm
 mass(cb::CelestialBody) = cb.gm / GRAVITATIONAL_CONSTANT
 naif_id(cb::CelestialBody) = cb.naif_id
-shape(cb::CelestialBody) = cb.shape
-radius(cb::CelestialBody) = cb |> shape |> radius
-volume(cb::CelestialBody) = cb |> shape |> volume
-density(cb::CelestialBody) = mass(cb) / radius(cb)
