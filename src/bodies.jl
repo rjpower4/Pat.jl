d190c26c7b1e501f034ab659a21bcd87b695d3ee
Rolfe Power <rolfepower4@gmail.com>
2021-12-06 10:50:30 -0500

    Add documentation

--- src/bodies.jl
+++ src/bodies.jl
@@ -17,7 +17,28 @@ CelestialBody(gm) = CelestialBody(gm, -1)
 
 """
     CelestialBody(gm, naif_id, radius::Float64) 
+
+Get the gravitational parameter of the body, often denoted μ.
 """
 gravitational_parameter(cb::CelestialBody) = cb.gm
+
+"""
+    mass(::CelestialBody)
+
+Return the mass of the body calculated as μ/G.
+
+!!! note
+
+    The mass calculation is performed by dividing the gravitational parameter
+    by the universal gravitational constant, G. The uncertainty in the
+    gravitational constant is much greater than that in the body's gravitational
+    parameter. Therefore, try to use the parameter when possible.
+"""
 mass(cb::CelestialBody) = cb.gm / GRAVITATIONAL_CONSTANT
+
+"""
+    naif_id(::CelestialBody)
+
+Return the NAIF ID of the body
+"""
 naif_id(cb::CelestialBody) = cb.naif_id
